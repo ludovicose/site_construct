bff5ddea8429e652a8a70b5145cea54a906b0919
Айбек <aibek.muratbekov@gmail.com>
2021-04-09 12:44:37 +0600

    Правки

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import {model} from "./model";
+import {Site} from "./classes/site";
+import {Sidebar} from "./classes/sidebar";
 import './styles/main.css'
+import {block} from "./utils";
 
-const $site = document.querySelector('#site')
+const site = new Site('#site')
+site.render(model)
 
-model.forEach(block => {
-    $site.insertAdjacentHTML('beforeend', block.toHtml())
 
+new Sidebar('#panel',newBlock =>{
+   model.push(newBlock)
+   site.render(model)
+})
